b41db15d1da5b33c2de54fccbbc78666311a6bac
Gargi <98906186+GargiMittal@users.noreply.github.com>
2023-08-05 01:38:01 +0530

    Add files via upload

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -48,23 +48,25 @@ def resume_song(status: StringVar):
     mixer.music.unpause()
     status.set("Song RESUMED")
 
-
+button_bg = "SteelBlue1"
+button_fg = "white"
+frame_bg = "LightSteelBlue1"
 
 
 # All the frames
-song_frame = LabelFrame(root, text='Current Song', bg="MediumPurple1" , width=400, height=80)
+song_frame = LabelFrame(root, text='Current Song', bg=frame_bg , width=400, height=80)
 song_frame.place(x=0, y=0)
 
-button_frame = LabelFrame(root, text='Control Buttons', bg='purple3', width=400, height=120)
+button_frame = LabelFrame(root, text='Control Buttons', bg=frame_bg, width=400, height=120)
 button_frame.place(y=80)
 
-listbox_frame = LabelFrame(root, text='Playlist', bg='purple4')
+listbox_frame = LabelFrame(root, text='Playlist', bg=frame_bg)
 listbox_frame.place(x=400, y=0, height=200, width=300)
 
 # All StringVar variables
-current_song = StringVar(root, value='<Not selected>')
+current_song = StringVar(root, value='Not selected')
 
-song_status = StringVar(root, value='<Not Available>')
+song_status = StringVar(root, value='Not Available')
 
 
 
@@ -83,34 +85,34 @@ playlist.pack(fill=BOTH, padx=5, pady=5)
 
 
 # SongFrame Labels
-Label(song_frame, text='CURRENTLY PLAYING:', bg='snow3', font=('Times', 10, 'bold')).place(x=5, y=20)
+Label(song_frame, text='CURRENTLY PLAYING:', bg=frame_bg, font=('Times', 10, 'bold')).place(x=5, y=20)
 
-song_lbl = Label(song_frame, textvariable=current_song, bg='snow3', font=("Times", 12), width=25)
+song_lbl = Label(song_frame, textvariable=current_song, bg=button_fg, font=("Times", 12), width=25)
 song_lbl.place(x=150, y=20)
 
 
 # Buttons in the main screen
-pause_btn = Button(button_frame, text='Pause', bg='snow3', font=("Georgia", 13), width=7,
+pause_btn = Button(button_frame, text='Pause', bg=button_fg, font=("Georgia", 13), width=7,
                     command=lambda: pause_song(song_status))
 pause_btn.place(x=15, y=10)
 
-stop_btn = Button(button_frame, text='Stop', bg='snow3', font=("Georgia", 13), width=7,
+stop_btn = Button(button_frame, text='Stop', bg=button_fg, font=("Georgia", 13), width=7,
                   command=lambda: stop_song(song_status))
 stop_btn.place(x=105, y=10)
 
-play_btn = Button(button_frame, text='Play', bg='snow3', font=("Georgia", 13), width=7,
+play_btn = Button(button_frame, text='Play', bg=button_fg, font=("Georgia", 13), width=7,
                   command=lambda: play_song(current_song, playlist, song_status))
 play_btn.place(x=195, y=10)
 
-resume_btn = Button(button_frame, text='Resume', bg='snow3', font=("Georgia", 13), width=7,
+resume_btn = Button(button_frame, text='Resume', bg=button_fg, font=("Georgia", 13), width=7,
                     command=lambda: resume_song(song_status))
 resume_btn.place(x=285, y=10)
 
-load_btn = Button(button_frame, text='Load Directory', bg='snow3', font=("Georgia", 13), width=35, command=lambda: load(playlist))
+load_btn = Button(button_frame, text='Load Directory', bg=button_fg, font=("Georgia", 13), width=35, command=lambda: load(playlist))
 load_btn.place(x=10, y=55)
 
 # Label at the bottom that displays the state of the music
-Label(root, textvariable=song_status, bg='snow', font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
+Label(root, textvariable=song_status, bg=button_fg, font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
 
 
 # Finalizing the GUI
